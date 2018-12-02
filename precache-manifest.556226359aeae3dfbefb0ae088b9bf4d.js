23b328b21b7ef66a5ea78fbd77f5b5fbf3388657
wooglie <imagas96@gmail.com>
2018-12-02 04:55:07 +0100

    first

---
+++ precache-manifest.556226359aeae3dfbefb0ae088b9bf4d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae489230b12547f9effa86896272d41d",
+    "url": "/static/media/timeline-hrv-dizajna.ae489230.png"
+  },
+  {
+    "revision": "eb93afc031e0a73827b359b85842e6d5",
+    "url": "/static/media/steady.eb93afc0.png"
+  },
+  {
+    "revision": "1d405a315f2d5255d99a911ea3792984",
+    "url": "/static/media/person.1d405a31.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4935f37b07b0f308c6af",
+    "url": "/static/js/main.4935f37b.chunk.js"
+  },
+  {
+    "revision": "6de05b54fec78e5191a7",
+    "url": "/static/js/1.6de05b54.chunk.js"
+  },
+  {
+    "revision": "4935f37b07b0f308c6af",
+    "url": "/static/css/main.c93cb5c3.chunk.css"
+  },
+  {
+    "revision": "db1dce456aca6015f470c272203628c2",
+    "url": "/index.html"
+  }
+];
